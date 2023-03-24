5c660535569ef83134f970db6f6b9b195f1fdefa
FunkeDer <jafter@ukr.net>
2023-03-24 11:18:03 +0200

    second commit

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
 let user_input_time = document.getElementById("user_input_time")
-let  = document.getElementById("button")
+let button = document.getElementById("button")
+
+let previousTimer = null; // initialize previous timer variable
 
 function consoleLogFunction(){
+  let semicircleRed = document.getElementById('semicircle-red')
+  let semicircleBlue = document.getElementById('semicircle-blue')
+  let semicircleWhite = document.getElementById('semicircle-white')
+  let time = document.getElementById('user_input_time')
+  let timer = document.getElementById('timer')
 
-let semicircleRed = document.getElementById('semicircle-red')
-let semicircleBlue = document.getElementById('semicircle-blue')
-let semicircleWhite = document.getElementById('semicircle-white')
-let time = document.getElementById('user_input_time')
-let timer = document.getElementById('timer')
+  let hr = 0
+  let min = 0
+  let sec = time.value
 
-let hr = 0
-let min = 0
-let sec = Number(time.value)
+  let hours = hr * 360000
+  let minutes = min * 60000
+  let seconds = sec * 1000
+  let setTime = hours + minutes + seconds
+  let startTime = Date.now()
+  let futureTime = startTime + setTime
 
-let hours = hr * 360000
-let minutes = min * 60000
-let seconds = sec * 1000
-let setTime = hours + minutes + seconds
-let startTime = Date.now()
-let futureTime = startTime + setTime
+  // clear previous timer loop
+  if (previousTimer) {
+    clearInterval(previousTimer);
+    previousTimer = null;
+  }
 
-let timerLoop = setInterval(couterDownTimer)
-couterDownTimer()
+  let timerLoop = setInterval(couterDownTimer)
+  couterDownTimer()
 
-function couterDownTimer(){
+  // if(time.value == ''){
+  //   alert('Enter time')
+  // }
+
+  function couterDownTimer(){
 
     let currentTime = Date.now()
     let remainingTime = futureTime - currentTime
     let angle = (remainingTime / setTime) * 360
-
-
-    if(timer.innerHTML = Math.round((remainingTime / 1000)) == 0){
-        timer.innerHTML = Math.round((remainingTime / 1000)).toLocaleString('en-US', {minimumFractionDigits:2, useGrouping: true})
-
-    }else{
-        timer.innerHTML = Math.round((remainingTime / 1000))
-    }
-
+      
+    timer.innerHTML = Math.round((remainingTime / 1000))*(+1)
 
     if(angle > 180){
-        semicircleRed.style.transform = 'rotate(180deg)'
-        semicircleBlue.style.transform = `rotate(${angle}deg)`
-        semicircleWhite.style.display = 'none'
+      semicircleRed.style.transform = 'rotate(180deg)'
+      semicircleBlue.style.transform = `rotate(${angle}deg)`
+      semicircleWhite.style.display = 'none'
     }else{
-        semicircleBlue.style.transform = `rotate(${angle}deg)`
-        semicircleRed.style.transform = `rotate(${angle}deg)`
-        semicircleWhite.style.display = 'block'
+      semicircleBlue.style.transform = `rotate(${angle}deg)`
+      semicircleRed.style.transform = `rotate(${angle}deg)`
+      semicircleWhite.style.display = 'block'
     }
 
-    if(remainingTime<0){
-        clearInterval(timerLoop)
-        // semicircleBlue.style.display = 'none'
-        // semicircleRed.style.display = 'none'
-        // semicircleWhite.style.display = 'none'
+    if(remainingTime < 0){
+      clearInterval(timerLoop)
+      alert('time is out')
     }
+  }
 
-    if (!time) {
-        alert('Error');
-      }
-
+  // update previous timer variable
+  previousTimer = timerLoop;
 }
+
+button.addEventListener('click', consoleLogFunction);
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      couterDownTimer();
-    }
-  });
-
-
-}
+  if (event.key === 'Enter') {
+    consoleLogFunction();
+  }
+});
